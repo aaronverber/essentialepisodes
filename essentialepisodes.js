a0e7d606ffafc311224d7c3bac207640b1c8d5bf
Aaron Verber <aaron.verber@gmail.com>
2015-05-04 21:41:08 -0700

    API test

--- essentialepisodes.js
+++ essentialepisodes.js
@@ -6,4 +6,33 @@ if (Meteor.isClient) {
       return Series.find({});
     }
   });
+
+  Meteor.call("authTVDB", function(err, response){
+    console.log("RESPONSE", err, response);
+  });
+}
+
+if(Meteor.isServer){
+  Meteor.methods({
+      authTVDB: function(){
+        this.unblock();
+        try{
+          var result = HTTP.call("POST", "https://api-dev.thetvdb.com/login", {
+            data: {
+              "apikey": "E6247B9FBD3BC9A5",
+              "username": "averber",
+              "userpass": "Kirk102810Picard"
+            },
+            headers: {
+              "Content-Type" : "application/json"
+            }
+          });
+          console.log("RESULT", result);
+        return true;
+      } catch(e){
+        console.log("ERROR", e);
+        return false;
+      }
+    }
+  });
 }
